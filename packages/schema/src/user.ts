15102fdc5d0da1dccab5111bc7a987da568e84a4
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-08-29 23:08:34 +0900

    ✨ feat:schema

---
+++ packages/schema/src/user.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const UserResponse = z.object({
+  id: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+});
+
+export type UserResponse = z.infer<typeof UserResponse>;
