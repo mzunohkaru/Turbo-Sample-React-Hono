c458706c9230d52297a8cef8e07af6b920ab7c18
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-08-30 23:30:16 +0900

    add:schema post

---
+++ packages/schema/src/post.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const PostResponse = z.object({
+  id: z.string(),
+  title: z.string(),
+  content: z.string(),
+});
+
+export type PostResponse = z.infer<typeof PostResponse>;
