f535a936f20c928bd5ab4fc118653799461f5d69
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-08-30 23:30:30 +0900

    add:fetch hook

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,98 +1,48 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { useUser } from "@/hook/useUser";
+import { usePost } from "@/hook/usePost";
 
 export default function Home() {
+  const {
+    userData,
+    fetchUser,
+    isMutating,
+    error,
+    createUserData,
+    createUser,
+  } = useUser();
+  const handleFetchUser = () => {
+    fetchUser();
+    console.log("userData", userData);
+  };
+
+  const handleCreateUser = () => {
+    createUser();
+    console.log("createUserData", createUserData);
+  };
+
+  const { postData } = usePost();
+  console.log("postData", postData);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <button onClick={() => handleFetchUser()}>Fetch User</button>
+        <button onClick={() => handleCreateUser()}>Create User</button>
+        <div>
+          <p>userData: {JSON.stringify(userData)}</p>
+          <p>isMutating: {JSON.stringify(isMutating)}</p>
+          <p>error: {JSON.stringify(error)}</p>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
       </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div>
+          <p>postData: {JSON.stringify(postData)}</p>
+        </div>
       </footer>
     </div>
   );
---
+++ apps/client/src/hook/usePost.ts
@@ -0,0 +1,41 @@
+import swr from "swr";
+import { AxiosError } from "axios";
+
+import { getFetcher } from "@/lib/fetch";
+import { PostResponse } from "@repo/schema";
+
+const url = "http://localhost:8080/api/posts";
+
+const fetcher = async (url: string) => {
+  try {
+    const res = await getFetcher<PostResponse>(url);
+    return res;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export function usePost() {
+  const { data: postData, error } = swr(url, fetcher, {
+    onSuccess: (data) => {
+      console.log("success", data);
+    },
+    onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+      if (retryCount > 1) return;
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401) return;
+      }
+      if (error.response?.status === 404) return;
+
+      console.log("retry", error);
+    },
+    shouldRetryOnError: true,
+    // dev
+    errorRetryInterval: 2000,
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+  return { postData, error };
+}
