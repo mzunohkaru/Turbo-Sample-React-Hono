3a7359aa131f3d2f5497552a3c7bb024d2a7f28f
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-08-31 20:27:56 +0900

    fix:lint

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -7,37 +7,37 @@ import { useUser } from "@/hook/useUser";
 import { usePost } from "@/hook/usePost";
 
 export default function Home() {
-  const { userData, fetchUser, isMutating, error, createUserData, createUser } =
-    useUser();
-  const handleFetchUser = () => {
-    fetchUser();
-    console.log("userData", userData);
-  };
+	const { userData, fetchUser, isMutating, error, createUserData, createUser } =
+		useUser();
+	const handleFetchUser = () => {
+		fetchUser();
+		console.log("userData", userData);
+	};
 
-  const handleCreateUser = () => {
-    createUser();
-    console.log("createUserData", createUserData);
-  };
+	const handleCreateUser = () => {
+		createUser();
+		console.log("createUserData", createUserData);
+	};
 
-  const { postData } = usePost();
-  console.log("postData", postData);
+	const { postData } = usePost();
+	console.log("postData", postData);
 
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <button onClick={() => handleFetchUser()}>Fetch User</button>
-        <button onClick={() => handleCreateUser()}>Create User</button>
-        <div>
-          <p>userData: {JSON.stringify(userData)}</p>
-          <p>isMutating: {JSON.stringify(isMutating)}</p>
-          <p>error: {JSON.stringify(error)}</p>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <div>
-          <p>postData: {JSON.stringify(postData)}</p>
-        </div>
-      </footer>
-    </div>
-  );
+	return (
+		<div className={styles.page}>
+			<main className={styles.main}>
+				<button onClick={() => handleFetchUser()}>Fetch User</button>
+				<button onClick={() => handleCreateUser()}>Create User</button>
+				<div>
+					<p>userData: {JSON.stringify(userData)}</p>
+					<p>isMutating: {JSON.stringify(isMutating)}</p>
+					<p>error: {JSON.stringify(error)}</p>
+				</div>
+			</main>
+			<footer className={styles.footer}>
+				<div>
+					<p>postData: {JSON.stringify(postData)}</p>
+				</div>
+			</footer>
+		</div>
+	);
 }
--- apps/client/src/hook/useUser.ts
+++ apps/client/src/hook/useUser.ts
@@ -7,63 +7,63 @@ import { UserResponse } from "@repo/schema";
 const url = "http://localhost:8080/api/users";
 
 const fetcher = async (url: string) => {
-  try {
-    const res = await getFetcher<UserResponse[]>(url);
-    return res;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+	try {
+		const res = await getFetcher<UserResponse[]>(url);
+		return res;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
 };
 
 const createFetcher = async (url: string) => {
-  try {
-    const res = await postFetcher<UserResponse, UserResponse>(url, {
-      id: "1",
-      name: "John Doe",
-      email: "john.doe@example.com",
-    });
-    return res;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
+	try {
+		const res = await postFetcher<UserResponse, UserResponse>(url, {
+			id: "1",
+			name: "John Doe",
+			email: "john.doe@example.com",
+		});
+		return res;
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
 };
 
 export function useUser() {
-  const {
-    data: userData,
-    trigger: fetchUser,
-    isMutating,
-    error,
-  } = useSWRMutation(url, fetcher, {
-    onSuccess: (data) => {
-      console.log("success", data);
-    },
-    onError(err, key, config) {
-      console.log("error", err);
-    },
-  });
+	const {
+		data: userData,
+		trigger: fetchUser,
+		isMutating,
+		error,
+	} = useSWRMutation(url, fetcher, {
+		onSuccess: (data) => {
+			console.log("success", data);
+		},
+		onError(err, key, config) {
+			console.log("error", err);
+		},
+	});
 
-  const { data: createUserData, trigger: createUser } = useSWRMutation(
-    url,
-    createFetcher,
-    {
-      onSuccess: (data) => {
-        console.log("success", data);
-      },
-      onError(err, key, config) {
-        console.log("error", err);
-      },
-    },
-  );
+	const { data: createUserData, trigger: createUser } = useSWRMutation(
+		url,
+		createFetcher,
+		{
+			onSuccess: (data) => {
+				console.log("success", data);
+			},
+			onError(err, key, config) {
+				console.log("error", err);
+			},
+		},
+	);
 
-  return {
-    userData,
-    fetchUser,
-    isMutating,
-    error,
-    createUserData,
-    createUser,
-  };
+	return {
+		userData,
+		fetchUser,
+		isMutating,
+		error,
+		createUserData,
+		createUser,
+	};
 }
