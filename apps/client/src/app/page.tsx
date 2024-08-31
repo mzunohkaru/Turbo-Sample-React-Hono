e230fa8a2815c643d4c8e1f45220b78baa19072b
mizuno-hikaru <mizuno.hikaru.01@gmail.com>
2024-08-31 20:37:36 +0900

    lint:client

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,43 +1,43 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-import { useUser } from "@/hook/useUser";
-import { usePost } from "@/hook/usePost";
+import Image from 'next/image';
+import { Button } from '@repo/ui/button';
+import styles from './page.module.css';
+import { useUser } from '@/hook/useUser';
+import { usePost } from '@/hook/usePost';
 
 export default function Home() {
-	const { userData, fetchUser, isMutating, error, createUserData, createUser } =
-		useUser();
-	const handleFetchUser = () => {
-		fetchUser();
-		console.log("userData", userData);
-	};
+  const { userData, fetchUser, isMutating, error, createUserData, createUser } =
+    useUser();
+  const handleFetchUser = () => {
+    fetchUser();
+    console.log('userData', userData);
+  };
 
-	const handleCreateUser = () => {
-		createUser();
-		console.log("createUserData", createUserData);
-	};
+  const handleCreateUser = () => {
+    createUser();
+    console.log('createUserData', createUserData);
+  };
 
-	const { postData } = usePost();
-	console.log("postData", postData);
+  const { postData } = usePost();
+  console.log('postData', postData);
 
-	return (
-		<div className={styles.page}>
-			<main className={styles.main}>
-				<button onClick={() => handleFetchUser()}>Fetch User</button>
-				<button onClick={() => handleCreateUser()}>Create User</button>
-				<div>
-					<p>userData: {JSON.stringify(userData)}</p>
-					<p>isMutating: {JSON.stringify(isMutating)}</p>
-					<p>error: {JSON.stringify(error)}</p>
-				</div>
-			</main>
-			<footer className={styles.footer}>
-				<div>
-					<p>postData: {JSON.stringify(postData)}</p>
-				</div>
-			</footer>
-		</div>
-	);
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <button onClick={() => handleFetchUser()}>Fetch User</button>
+        <button onClick={() => handleCreateUser()}>Create User</button>
+        <div>
+          <p>userData: {JSON.stringify(userData)}</p>
+          <p>isMutating: {JSON.stringify(isMutating)}</p>
+          <p>error: {JSON.stringify(error)}</p>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <div>
+          <p>postData: {JSON.stringify(postData)}</p>
+        </div>
+      </footer>
+    </div>
+  );
 }
--- apps/client/src/lib/fetch.ts
+++ apps/client/src/lib/fetch.ts
@@ -1,96 +1,96 @@
 import axios, {
-	type Method,
-	type AxiosRequestConfig,
-	type AxiosResponse,
-} from "axios";
+  type Method,
+  type AxiosRequestConfig,
+  type AxiosResponse,
+} from 'axios';
 
 const makeRequest = async <T>(config: AxiosRequestConfig): Promise<T> => {
-	try {
-		const response: AxiosResponse<T> = await axios({
-			headers: { "Content-Type": "application/json" },
-			...config,
-		});
-		return response.data;
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
+  try {
+    const response: AxiosResponse<T> = await axios({
+      headers: { 'Content-Type': 'application/json' },
+      ...config,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 export async function fetcherStatus<T, U = never>(
-	url: string,
-	method: Method,
-	data: T,
-	params?: U,
-	config?: AxiosRequestConfig,
+  url: string,
+  method: Method,
+  data: T,
+  params?: U,
+  config?: AxiosRequestConfig,
 ): Promise<number> {
-	try {
-		const requestConfig = {
-			method: method,
-			data: { ...data },
-			params,
-			paramsSerializer: { indexes: null },
-			url,
-			...config,
-		};
-		const response: AxiosResponse = await axios({
-			headers: { "Content-Type": "application/json" },
-			...requestConfig,
-		});
-		return response.status;
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
+  try {
+    const requestConfig = {
+      method: method,
+      data: { ...data },
+      params,
+      paramsSerializer: { indexes: null },
+      url,
+      ...config,
+    };
+    const response: AxiosResponse = await axios({
+      headers: { 'Content-Type': 'application/json' },
+      ...requestConfig,
+    });
+    return response.status;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export async function getFetcher<T, U = never>(
-	url: string,
-	params?: U,
-	config?: AxiosRequestConfig,
+  url: string,
+  params?: U,
+  config?: AxiosRequestConfig,
 ): Promise<T> {
-	return await makeRequest<T>({
-		method: "GET",
-		params,
-		paramsSerializer: { indexes: null },
-		url,
-		...config,
-	});
+  return await makeRequest<T>({
+    method: 'GET',
+    params,
+    paramsSerializer: { indexes: null },
+    url,
+    ...config,
+  });
 }
 
 export async function postFetcher<T, U>(
-	url: string,
-	data: U,
-	config?: AxiosRequestConfig,
+  url: string,
+  data: U,
+  config?: AxiosRequestConfig,
 ): Promise<T> {
-	return await makeRequest<T>({
-		method: "POST",
-		data,
-		url,
-		...config,
-	});
+  return await makeRequest<T>({
+    method: 'POST',
+    data,
+    url,
+    ...config,
+  });
 }
 
 export async function putFetcher<T, U = never>(
-	url: string,
-	data?: U,
-	config?: AxiosRequestConfig,
+  url: string,
+  data?: U,
+  config?: AxiosRequestConfig,
 ): Promise<T> {
-	return await makeRequest<T>({
-		method: "PUT",
-		data,
-		url,
-		...config,
-	});
+  return await makeRequest<T>({
+    method: 'PUT',
+    data,
+    url,
+    ...config,
+  });
 }
 
 export async function deleteFetcher<T>(
-	url: string,
-	config?: AxiosRequestConfig,
+  url: string,
+  config?: AxiosRequestConfig,
 ): Promise<T> {
-	return await makeRequest<T>({
-		method: "DELETE",
-		url,
-		...config,
-	});
+  return await makeRequest<T>({
+    method: 'DELETE',
+    url,
+    ...config,
+  });
 }
